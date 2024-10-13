9a089acebc607051919de62f6390594394206622
wanditt <79541813+wanditt@users.noreply.github.com>
2024-10-12 23:17:08 -0400

    Fix build issues

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,39 +1,79 @@
-import React from 'react'
+import React from "react";
 
-import logo from '../../images/icons/logo-sm.png'
-import search from '../../images/icons/search-icon-sm.png'
-import cart from '../../images/icons/cart-sm.png'
-
+import logo from "../../images/icons/logo-sm.png";
+import search from "../../images/icons/search-icon-sm.png";
+import cart from "../../images/icons/cart-sm.png";
 
 function Header() {
-    return (
-        <div className="nav-wrapper fixed-top">
-		<div className="container">
-			<nav className="navbar navbar-toggleable-sm navbar-expand-md">
-			    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-			        ☰
-			    </button>
-			    <a className="navbar-brand mx-auto" href="#"><img src={logo}alt=" " /></a>
+  return (
+    <div className="nav-wrapper fixed-top">
+      <div className="container">
+        <nav className="navbar navbar-toggleable-sm navbar-expand-md">
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target=".navbar-collapse"
+          >
+            ☰
+          </button>
+          <a className="navbar-brand mx-auto" href="#">
+            <img src={logo} alt="Company logo" />
+          </a>
 
-			    <div className="navbar-collapse collapse">
-			        <ul className="navbar-nav nav-justified w-100 nav-fill">
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="/mac/">Mac</a></li>
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">iphone</a></li>
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">ipad</a></li>
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">watch</a></li>
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">tv</a></li>
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">Music</a></li>
-						<li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">Support</a></li>
-						<li className="nav-item" ><a className="nav-link js-scroll-trigger" href="/search/"><img src={search} alt=" " /></a></li>
-						<li className="nav-item"><a  className="nav-link js-scroll-trigger" href="/cart/"><img src={cart} alt=" " /></a></li>
-			        </ul>
-			    </div>
-			</nav>
-		</div>
-	</div>
-            
-       
-    )
+          <div className="navbar-collapse collapse">
+            <ul className="navbar-nav nav-justified w-100 nav-fill">
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="/mac/">
+                  Mac
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  iPhone
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  iPad
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  Watch
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  TV
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  Music
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  Support
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="/search/">
+                  <img src={search} alt="Search icon" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="/cart/">
+                  <img src={cart} alt="Cart icon" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
